e6a27dfeb2f1f20ff05a1388f4d3757d1867d44f
Robin Stolpe <robin@stolpe.io>
2022-01-25 08:54:57 +0100

    Fixed license in everyfile

--- .universal/loginPage.ps1
+++ .universal/loginPage.ps1
@@ -1,1 +1,19 @@
-﻿New-PSULoginPage -Image "/pictures/" -Title "Franky Beta 1" -PrimaryColor "white" -SecondaryColor "white" -SecondaryFontColor "black" -HeaderFontColor "black" -HeaderColor "rgba(0, 151, 207, 0.6)" -Copyright "© 2022 - Stolpe.io"
+﻿<#
+    Copyright (C) 2022  KeepCodeOpen - The ultimate IT-Support dashboard
+    <https://keepcodeopen.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#>
+
+New-PSULoginPage -Image "/pictures/" -Title "Franky Beta 1" -PrimaryColor "white" -SecondaryColor "white" -SecondaryFontColor "black" -HeaderFontColor "black" -HeaderColor "rgba(0, 151, 207, 0.6)" -Copyright "© 2022 - Stolpe.io"
